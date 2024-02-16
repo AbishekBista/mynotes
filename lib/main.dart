779d630f0bc3e03c4fb815e5b06bd7755caadaa7
AbishekBista <abishekbista84@gmail.com>
2024-02-16 22:58:16 +0545

    step-11

--- lib/main.dart
+++ lib/main.dart
@@ -1,139 +1,53 @@
-import 'package:mynotes/services/auth/auth_exceptions.dart';
-import 'package:mynotes/services/auth/auth_provider.dart';
-import 'package:mynotes/services/auth/auth_user.dart';
-import 'package:test/test.dart';
+import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/notes_view.dart';
+import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/views/verify_email_view.dart';
 
 void main() {
-  group('Mock Authentication', () {
-    final provider = MockAuthProvider();
-    test('Should not be initialized to begin with', () {
-      expect(provider.isInitialized, false);
-    });
-
-    test('Cannot log out if not initialized', () {
-      expect(
-        provider.logOut(),
-        throwsA(const TypeMatcher<NotInitializedException>()),
-      );
-    });
-
-    test('Should be able to be initialized', () async {
-      await provider.initialize();
-      expect(provider.isInitialized, true);
-    });
-
-    test('User should be null after initialization', () {
-      expect(provider.currentUser, null);
-    });
-
-    test(
-      'Should be able to initialize in less than 2 seconds',
-      () async {
-        await provider.initialize();
-        expect(provider.isInitialized, true);
-      },
-      timeout: const Timeout(Duration(seconds: 2)),
-    );
-
-    test('Create user should delegate to logIn function', () async {
-      final badEmailUser = provider.createUser(
-        email: 'foo@bar.com',
-        password: 'anypassword',
-      );
-
-      expect(badEmailUser,
-          throwsA(const TypeMatcher<UserNotFoundAuthException>()));
-
-      final badPasswordUser = provider.createUser(
-        email: 'someone@bar.com',
-        password: 'foobar',
-      );
-      expect(badPasswordUser,
-          throwsA(const TypeMatcher<WrongPasswordAuthException>()));
-
-      final user = await provider.createUser(
-        email: 'foo',
-        password: 'bar',
-      );
-      expect(provider.currentUser, user);
-      expect(user.isEmailVerified, false);
-    });
-
-    test('Logged in user should be able to get verified', () {
-      provider.sendEmailVerification();
-      final user = provider.currentUser;
-      expect(user, isNotNull);
-      expect(user!.isEmailVerified, true);
-    });
-
-    test('Should be able to log out and log in again', () async {
-      await provider.logOut();
-      await provider.logIn(
-        email: 'email',
-        password: 'password',
-      );
-      final user = provider.currentUser;
-      expect(user, isNotNull);
-    });
-  });
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      useMaterial3: true,
+    ),
+    home: const HomePage(),
+    routes: {
+      loginRoute: (context) => const LoginView(),
+      registerRoute: (context) => const RegisterView(),
+      notesRoute: (context) => const NotesView(),
+      verifyEmailRoute: (context) => const VerifyEmailView()
+    },
+  ));
 }
 
-class NotInitializedException implements Exception {}
-
-class MockAuthProvider implements AuthProvider {
-  AuthUser? _user;
-  var _isInitialized = false;
-  bool get isInitialized => _isInitialized;
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
-  Future<AuthUser> createUser({
-    required String email,
-    required String password,
-  }) async {
-    if (!isInitialized) throw NotInitializedException();
-    await Future.delayed(const Duration(seconds: 1));
-    return logIn(
-      email: email,
-      password: password,
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: AuthService.firebase().initialize(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = AuthService.firebase().currentUser;
+            if (user != null) {
+              if (user.isEmailVerified) {
+                return const NotesView();
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
     );
-  }
-
-  @override
-  AuthUser? get currentUser => _user;
-
-  @override
-  Future<void> initialize() async {
-    await Future.delayed(const Duration(seconds: 1));
-    _isInitialized = true;
-  }
-
-  @override
-  Future<AuthUser> logIn({
-    required String email,
-    required String password,
-  }) {
-    if (!isInitialized) throw NotInitializedException();
-    if (email == 'foo@bar.com') throw UserNotFoundAuthException();
-    if (password == 'foobar') throw WrongPasswordAuthException();
-    const user = AuthUser(isEmailVerified: false);
-    _user = user;
-    return Future.value(user);
-  }
-
-  @override
-  Future<void> logOut() async {
-    if (!isInitialized) throw NotInitializedException();
-    if (_user == null) throw UserNotFoundAuthException();
-    await Future.delayed(const Duration(seconds: 1));
-    _user = null;
-  }
-
-  @override
-  Future<void> sendEmailVerification() async {
-    if (!isInitialized) throw NotInitializedException();
-    final user = _user;
-    if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true);
-    _user = newUser;
   }
 }
