cf0796ff3419b2c887dc0240ba6e24df90705f02
AbishekBista <abishekbista84@gmail.com>
2024-02-21 22:22:09 +0545

    step-17

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -7,7 +7,6 @@ import 'package:path/path.dart' show join;
 
 class NotesService {
   Database? _db;
-
   List<DatabaseNote> _notes = [];
 
   static final NotesService _shared = NotesService._sharedInstance();
@@ -19,7 +18,6 @@ class NotesService {
     );
   }
   factory NotesService() => _shared;
-
   late final StreamController<List<DatabaseNote>> _notesStreamController;
 
   Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
@@ -48,16 +46,13 @@ class NotesService {
   }) async {
     await _ensureDbIsOpen();
     final db = _getDatabaseOrThrow();
-
     // make sure note exists
     await getNote(id: note.id);
-
     // update DB
     final updatesCount = await db.update(noteTable, {
       textColumn: text,
       isSyncedWithCloudColumn: 0,
     });
-
     if (updatesCount == 0) {
       throw CouldNotUpdateNote();
     } else {
@@ -72,9 +67,7 @@ class NotesService {
   Future<Iterable<DatabaseNote>> getAllNotes() async {
     await _ensureDbIsOpen();
     final db = _getDatabaseOrThrow();
-
     final notes = await db.query(noteTable);
-
     return notes.map((noteRow) => DatabaseNote.fromRow(noteRow));
   }
 
@@ -87,7 +80,6 @@ class NotesService {
       where: 'id = ?',
       whereArgs: [id],
     );
-
     if (notes.isEmpty) {
       throw CouldNotFindNote();
     } else {
@@ -111,13 +103,12 @@ class NotesService {
   Future<void> deleteNote({required int id}) async {
     await _ensureDbIsOpen();
     final db = _getDatabaseOrThrow();
-    final deleteCount = await db.delete(
+    final deletedCount = await db.delete(
       noteTable,
       where: 'id = ?',
       whereArgs: [id],
     );
-
-    if (deleteCount == 0) {
+    if (deletedCount == 0) {
       throw CouldNotDeleteNote();
     } else {
       _notes.removeWhere((note) => note.id == id);
@@ -128,47 +119,38 @@ class NotesService {
   Future<DatabaseNote> createNote({required DatabaseUser owner}) async {
     await _ensureDbIsOpen();
     final db = _getDatabaseOrThrow();
-
-    // make sure the owner exists in the database with the correct id
+    // make sure owner exists in the database with the correct id
     final dbUser = await getUser(email: owner.email);
-
     if (dbUser != owner) {
       throw CouldNotFindUser();
     }
-
     const text = '';
-
     // create the note
     final noteId = await db.insert(noteTable, {
       userIdColumn: owner.id,
       textColumn: text,
       isSyncedWithCloudColumn: 1,
     });
-
     final note = DatabaseNote(
       id: noteId,
       userId: owner.id,
       text: text,
       isSyncedWithCloud: true,
     );
-
     _notes.add(note);
     _notesStreamController.add(_notes);
-
     return note;
   }
 
   Future<DatabaseUser> getUser({required String email}) async {
     await _ensureDbIsOpen();
     final db = _getDatabaseOrThrow();
-
     final results = await db.query(
       userTable,
       limit: 1,
       where: 'email = ?',
       whereArgs: [email.toLowerCase()],
     );
-
     if (results.isEmpty) {
       throw CouldNotFindUser();
     } else {
@@ -188,11 +170,13 @@ class NotesService {
     if (results.isNotEmpty) {
       throw UserAlreadyExists();
     }
-
-    final userId =
-        await db.insert(userTable, {emailColumn: email.toLowerCase()});
-
-    return DatabaseUser(id: userId, email: email);
+    final userId = await db.insert(userTable, {
+      emailColumn: email.toLowerCase(),
+    });
+    return DatabaseUser(
+      id: userId,
+      email: email,
+    );
   }
 
   Future<void> deleteUser({required String email}) async {
@@ -203,7 +187,6 @@ class NotesService {
       where: 'email = ?',
       whereArgs: [email.toLowerCase()],
     );
-
     if (deletedCount != 1) {
       throw CouldNotDeleteUser();
     }
@@ -215,6 +198,16 @@ class NotesService {
       throw DatabaseIsNotOpen();
     } else {
       return db;
+    }
+  }
+
+  Future<void> close() async {
+    final db = _db;
+    if (db == null) {
+      throw DatabaseIsNotOpen();
+    } else {
+      await db.close();
+      _db = null;
     }
   }
 
@@ -235,27 +228,13 @@ class NotesService {
       final dbPath = join(docsPath.path, dbName);
       final db = await openDatabase(dbPath);
       _db = db;
-
-      // create user table
-
+      // create the user table
       await db.execute(createUserTable);
-
       // create note table
-
       await db.execute(createNoteTable);
       await _cacheNotes();
     } on MissingPlatformDirectoryException {
       throw UnableToGetDocumentsDirectory();
-    }
-  }
-
-  Future<void> close() async {
-    final db = _db;
-    if (db == null) {
-      throw DatabaseIsNotOpen();
-    } else {
-      await db.close();
-      _db = null;
     }
   }
 }
@@ -264,22 +243,17 @@ class NotesService {
 class DatabaseUser {
   final int id;
   final String email;
-
   const DatabaseUser({
     required this.id,
     required this.email,
   });
-
   DatabaseUser.fromRow(Map<String, Object?> map)
       : id = map[idColumn] as int,
         email = map[emailColumn] as String;
-
   @override
   String toString() => 'Person, ID = $id, email = $email';
-
   @override
   bool operator ==(covariant DatabaseUser other) => id == other.id;
-
   @override
   int get hashCode => id.hashCode;
 }
@@ -289,28 +263,23 @@ class DatabaseNote {
   final int userId;
   final String text;
   final bool isSyncedWithCloud;
-
   DatabaseNote({
     required this.id,
     required this.userId,
     required this.text,
     required this.isSyncedWithCloud,
   });
-
   DatabaseNote.fromRow(Map<String, Object?> map)
       : id = map[idColumn] as int,
         userId = map[userIdColumn] as int,
         text = map[textColumn] as String,
         isSyncedWithCloud =
             (map[isSyncedWithCloudColumn] as int) == 1 ? true : false;
-
   @override
   String toString() =>
-      'Note, ID = $id, userId = $userId, isSyncedWithCloud = $isSyncedWithCloud';
-
+      'Note, ID = $id, userId = $userId, isSyncedWithCloud = $isSyncedWithCloud, text = $text';
   @override
   bool operator ==(covariant DatabaseNote other) => id == other.id;
-
   @override
   int get hashCode => id.hashCode;
 }
@@ -327,15 +296,12 @@ const createUserTable = '''CREATE TABLE IF NOT EXISTS "user" (
         "id"	INTEGER NOT NULL,
         "email"	TEXT NOT NULL UNIQUE,
         PRIMARY KEY("id" AUTOINCREMENT)
-      );
-      ''';
-const createNoteTable = '''CREATE TABLE "note" (
+      );''';
+const createNoteTable = '''CREATE TABLE IF NOT EXISTS "note" (
         "id"	INTEGER NOT NULL,
         "user_id"	INTEGER NOT NULL,
         "text"	TEXT,
         "is_synced_with_cloud"	INTEGER NOT NULL DEFAULT 0,
         FOREIGN KEY("user_id") REFERENCES "user"("id"),
         PRIMARY KEY("id" AUTOINCREMENT)
-      );
-
-      ''';
+      );''';
--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -7,89 +7,97 @@ import 'package:mynotes/utilities/dialogs/logout_dialog.dart';
 import 'package:mynotes/views/notes/notes_list_view.dart';
 
 class NotesView extends StatefulWidget {
-  const NotesView({super.key});
-
+  const NotesView({Key? key}) : super(key: key);
   @override
-  State<NotesView> createState() => _NotesViewState();
+  _NotesViewState createState() => _NotesViewState();
 }
 
 class _NotesViewState extends State<NotesView> {
   late final NotesService _notesService;
   String get userEmail => AuthService.firebase().currentUser!.email!;
-
   @override
   void initState() {
     _notesService = NotesService();
-    _notesService.open();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: const Text('Your Notes'),
-            backgroundColor: Colors.blue,
-            foregroundColor: Colors.white,
-            actions: [
-              IconButton(
-                onPressed: () {
-                  // on press logic
-                  Navigator.of(context).pushNamed(newNoteRoute);
-                },
-                icon: const Icon(Icons.add),
-              ),
-              PopupMenuButton<MenuAction>(
-                onSelected: (value) async {
-                  switch (value) {
-                    case MenuAction.logout:
-                      final shouldLogout = await showLogoutDialog(context);
-                      if (shouldLogout) {
-                        await AuthService.firebase().logOut();
-                        Navigator.of(context)
-                            .pushNamedAndRemoveUntil(loginRoute, (_) => false);
+      appBar: AppBar(
+        title: const Text('Your Notes'),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(createOrUpdateNoteRoute);
+            },
+            icon: const Icon(Icons.add),
+          ),
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogoutDialog(context);
+                  if (shouldLogout) {
+                    await AuthService.firebase().logOut();
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      loginRoute,
+                      (_) => false,
+                    );
+                  }
+              }
+            },
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: Text('Log out'),
+                ),
+              ];
+            },
+          )
+        ],
+      ),
+      body: FutureBuilder(
+        future: _notesService.getOrCreateUser(email: userEmail),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return StreamBuilder(
+                stream: _notesService.allNotes,
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                    case ConnectionState.active:
+                      if (snapshot.hasData) {
+                        final allNotes = snapshot.data as List<DatabaseNote>;
+                        return NotesListView(
+                          notes: allNotes,
+                          onDeleteNote: (note) async {
+                            await _notesService.deleteNote(id: note.id);
+                          },
+                          onTap: (note) {
+                            Navigator.of(context).pushNamed(
+                              createOrUpdateNoteRoute,
+                              arguments: note,
+                            );
+                          },
+                        );
+                      } else {
+                        return const CircularProgressIndicator();
                       }
-                      break;
+                    default:
+                      return const CircularProgressIndicator();
                   }
                 },
-                itemBuilder: (context) {
-                  return const [
-                    PopupMenuItem<MenuAction>(
-                        value: MenuAction.logout, child: Text('Logout'))
-                  ];
-                },
-              )
-            ]),
-        body: FutureBuilder(
-            future: _notesService.getOrCreateUser(email: userEmail),
-            builder: (context, snapshot) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.done:
-                  return StreamBuilder(
-                      stream: _notesService.allNotes,
-                      builder: (context, snapshot) {
-                        switch (snapshot.connectionState) {
-                          case ConnectionState.waiting:
-                          case ConnectionState.active:
-                            if (snapshot.hasData) {
-                              final allNotes =
-                                  snapshot.data as List<DatabaseNote>;
-                              return NotesListView(
-                                  notes: allNotes,
-                                  onDeleteNote: (note) async {
-                                    await _notesService.deleteNote(id: note.id);
-                                  });
-                            } else {
-                              return const CircularProgressIndicator();
-                            }
-                          default:
-                            return const CircularProgressIndicator();
-                        }
-                      });
-
-                default:
-                  return const CircularProgressIndicator();
-              }
-            }));
+              );
+            default:
+              return const CircularProgressIndicator();
+          }
+        },
+      ),
+    );
   }
 }
